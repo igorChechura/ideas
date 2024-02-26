345a424b8a79db1ae62c48585ed70ad4c42151d0
igorChechura <igorchechura@gmail.com>
2024-02-26 23:34:09 +0300

    feed and profile routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/feed', function () {
+    return view('feed');
+});
+
+Route::get('/profile', function () {
+    return view('users.profile');
+});
