8fdcd53f8bf6899aa6ad318451c0aa50abdea0c9
igorChechura <igorchechura@gmail.com>
2024-02-29 14:58:29 +0300

    Ideas model

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,27 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Idea;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        $users = [
-            [
-                'name' => 'Alex',
-                'age' => 30,
-            ],
-            [
-                'name' => 'Dan',
-                'age' => 25,
-            ],
-            [
-                'name' => 'John',
-                'age' => 17,
-            ],
-        ];
+        $idea = new Idea([
+            'content' => 'hello world',
+        ]);
+        $idea->save();
 
-        return view('dashboard', ['users' => $users]);
+        return view('dashboard', [
+            'ideas' => Idea::orderBy('created_at', 'DESC')->get()
+        ]);
     }
 }
