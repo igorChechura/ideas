67d1319464ea580bf0d695c64bc126c292cd0fe2
igorChechura <igorchechura@gmail.com>
2024-03-07 21:00:53 +0300

    Idea update method

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -15,16 +15,35 @@ class IdeaController extends Controller
     public function store()
     {
         request()->validate([
-            'idea' => 'required|min:5|max:240'
+            'content' => 'required|min:5|max:240'
         ]);
 
         $idea = Idea::create(
             [
-                'content' => request()->get('idea', ''),
+                'content' => request()->get('content', ''),
             ]
         );
 
         return redirect()->route('dashboard')->with('success', 'Idea created successfully.');
+    }
+
+    public function edit(Idea $idea)
+    {
+        $editing = true;
+
+        return view('ideas.show', compact('idea', 'editing'));
+    }
+
+    public function update(Idea $idea)
+    {
+        request()->validate([
+            'content' => 'required|min:5|max:240'
+        ]);
+
+        $idea->content = request()->get('content', '');
+        $idea->save();
+
+        return redirect()->route('ideas.show', $idea->id)->with('success', 'Idea updated successfully.');
     }
 
     public function destroy(Idea $idea)
