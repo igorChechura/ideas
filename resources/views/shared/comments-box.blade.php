3488052f6e6f63202bba4c29c3f0b0f21e9f7796
igorChechura <igorchechura@gmail.com>
2024-03-21 17:51:56 +0300

    Comment form created

--- resources/views/shared/comments-box.blade.php
+++ resources/views/shared/comments-box.blade.php
@@ -1,11 +1,13 @@
 <div>
-    <div class="mb-3">
-        <textarea class="fs-6 form-control" rows="1"></textarea>
-    </div>
-    <div>
-        <button class="btn btn-primary btn-sm"> Post Comment </button>
-    </div>
-
+    <form action="{{ route('ideas.comments.store', $idea->id) }}" method="POST">
+        @csrf
+        <div class="mb-3">
+            <textarea name="content" class="fs-6 form-control" rows="1"></textarea>
+        </div>
+        <div>
+            <button type="submit" class="btn btn-primary btn-sm"> Post Comment </button>
+        </div>
+    </form>
     <hr>
     <div class="d-flex align-items-start">
         <img style="width:35px" class="me-2 avatar-sm rounded-circle"
