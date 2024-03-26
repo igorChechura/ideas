69bc2dade042af2abc8656c092248a3d44006838
igorChechura <igorchechura@gmail.com>
2024-03-26 17:42:01 +0300

    Auth controller, html

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\IdeaController;
@@ -29,7 +30,14 @@ Route::put('/ideas/{idea}', [IdeaController::class, 'update'])->name('ideas.upda
 Route::delete('/ideas/{idea}', [IdeaController::class, 'destroy'])->name('ideas.destroy');
 
 
+
 Route::post('/ideas/{idea}/comments', [CommentController::class, 'store'])->name('ideas.comments.store');
+
+
+
+Route::get('/register', [AuthController::class, 'register'])->name('register');
+
+Route::post('/register', [AuthController::class, 'store']);
 
 
 Route::get('/terms', function () {
