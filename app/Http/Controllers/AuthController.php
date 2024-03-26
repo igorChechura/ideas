7886e69832fea1e7ccca9f0b3588886c663c7d8d
igorChechura <igorchechura@gmail.com>
2024-03-26 17:48:05 +0300

    Auth validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,6 +13,12 @@ class AuthController extends Controller
 
     public function store()
     {
-
+        $validated = request()->validate(
+            [
+                'name' => 'required|min:3|max:40',
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required|confirmed'
+            ]
+        );
     }
 }
